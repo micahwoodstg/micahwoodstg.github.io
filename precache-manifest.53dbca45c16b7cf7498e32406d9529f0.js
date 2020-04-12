370bafc4a4aebd9e2b80deb0b06df6757f80562e
Micah Wood <micah.wood@stgconsulting.com>
2020-04-12 14:47:28 -0600

    Add previous correct guesses

---
+++ precache-manifest.53dbca45c16b7cf7498e32406d9529f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd2966ee9a386fd8b861aa677367b8ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abdd5682e5ef9d3425d0",
+    "url": "/static/css/2.4af70dfe.chunk.css"
+  },
+  {
+    "revision": "4b6ed09b1f50d1ddc794",
+    "url": "/static/css/main.d7b91ac2.chunk.css"
+  },
+  {
+    "revision": "abdd5682e5ef9d3425d0",
+    "url": "/static/js/2.f6cff377.chunk.js"
+  },
+  {
+    "revision": "6d65dc0fd413da3aeeb59a6c141286d4",
+    "url": "/static/js/2.f6cff377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b6ed09b1f50d1ddc794",
+    "url": "/static/js/main.79589c40.chunk.js"
+  },
+  {
+    "revision": "ff9b8495fa6bcf452fc6",
+    "url": "/static/js/runtime-main.79a931ac.js"
+  }
+]);
